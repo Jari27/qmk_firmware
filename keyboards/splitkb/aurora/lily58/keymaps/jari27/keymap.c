a45e9093d055b7c93ff087ce1a02287814f2841c
Jari Meevis <jarimeevis@gmail.com>
2024-04-17 21:17:58 +0200

    remove layers on space/enter

--- keyboards/splitkb/aurora/lily58/keymaps/jari27/keymap.c
+++ keyboards/splitkb/aurora/lily58/keymaps/jari27/keymap.c
@@ -23,11 +23,11 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       [_DEF] = LAYOUT(
-          KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
-          KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-          KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, RCTL_T(KC_QUOT),
-          KC_LSFT, KC_Z,    X_CUT_,   C_COPY,  V_PASTE, KC_B,   OSM(MOD_MEH), OSM(MOD_HYPR), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                          OSM(MOD_LALT), KC_LGUI, MO(_SYM), LT(_SYM,KC_SPC), LT(_NAV,KC_ENT), MO(_NAV), KC_BSPC, KC_RGUI
+          KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                              KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
+          KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
+          KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, RCTL_T(KC_QUOT),
+          KC_LSFT, KC_Z,    X_CUT_,  C_COPY,  V_PASTE, KC_B, OSM(MOD_MEH), OSM(MOD_HYPR), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                          OSM(MOD_LALT), KC_LGUI, MO(_SYM),  KC_SPC,              KC_ENT, MO(_NAV), KC_BSPC, KC_RGUI
       ),
     //   [_GAMING] = LAYOUT(
     //       KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
@@ -36,25 +36,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	//                                    KC_LALT, KC_LALT, KC_SPC, KC_SPC,              KC_ENT, XXXXXXX, KC_BSPC, KC_RGUI
     //   ),
       [_SYM] = LAYOUT(
-          KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                               KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-          _______, KC_GRV,  KC_LT,   KC_GT,   KC_DQUO, KC_UNDS,                             KC_AMPR, KC_LBRC, KC_RBRC, DCSTRNG, KC_PERC, XXXXXXX,
-          _______, KC_EXLM, KC_MINS, KC_PLUS, KC_EQL,  KC_HASH,                             KC_PIPE, KC_LPRN, KC_RPRN, KC_COLN, KC_AT,   _______,
-          _______, KC_CIRC, KC_SLSH, KC_ASTR, KC_BSLS, FLDR_UP, _______,           _______, KC_TILD, KC_LCBR, KC_RCBR, KC_DLR,  KC_QUES, _______,
-                                     _______, _______, _______, _______,      LT(_ADJ,KC_ENT), MO(_ADJ),   _______, _______
+          KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                             KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+          _______, KC_GRV,  KC_LT,   KC_GT,   KC_DQUO, KC_UNDS,                           KC_AMPR, KC_LBRC, KC_RBRC, DCSTRNG, KC_PERC, XXXXXXX,
+          _______, KC_EXLM, KC_MINS, KC_PLUS, KC_EQL,  KC_HASH,                           KC_PIPE, KC_LPRN, KC_RPRN, KC_COLN, KC_AT,   _______,
+          _______, KC_CIRC, KC_SLSH, KC_ASTR, KC_BSLS, FLDR_UP, _______,         _______, KC_TILD, KC_LCBR, KC_RCBR, KC_DLR,  KC_QUES, _______,
+                                     _______, _______, _______, _______,          KC_ENT, MO(_ADJ),   _______, _______
       ),
       [_NAV] = LAYOUT(
-          KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                               KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-          _______, XXXXXXX, KC_BTN2, KC_MS_U, KC_BTN1, KC_WH_U,                             XXXXXXX, TAB_PRV, TAB_NXT, XXXXXXX, XXXXXXX, XXXXXXX,
-          _______, XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,                             KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, _______,
-          _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,           _______, KC_MPRV, KC_MNXT, KC_MPLY, XXXXXXX, XXXXXXX, _______,
-                                     _______, _______, MO(_ADJ),LT(_ADJ,KC_SPC),   _______, _______, _______, _______
+          KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                             KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+          _______, XXXXXXX, KC_BTN2, KC_MS_U, KC_BTN1, KC_WH_U,                           XXXXXXX, TAB_PRV, TAB_NXT, XXXXXXX, XXXXXXX, XXXXXXX,
+          _______, XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,                           KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, _______,
+          _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,         _______, KC_MPRV, KC_MNXT, KC_MPLY, XXXXXXX, XXXXXXX, _______,
+                                     _______, _______, MO(_ADJ),KC_SPC ,         _______, _______, _______, _______
       ),
       [_ADJ] = LAYOUT(
-          QK_BOOT, EE_CLR,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                             NK_TOGG, XXXXXXX, CG_TOGG, XXXXXXX, XXXXXXX, XXXXXXX,
-          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                             XXXXXXX, XXXXXXX, XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU,
-          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                             XXXXXXX, RGB_SPI, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI,
-          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,           XXXXXXX, XXXXXXX, RGB_SPD, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD,
-                                     _______, _______, _______, _______,           _______, _______, _______, _______
+          QK_BOOT, EE_CLR,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                           NK_TOGG, XXXXXXX, CG_TOGG, XXXXXXX, XXXXXXX, XXXXXXX,
+          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                           XXXXXXX, XXXXXXX, XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU,
+          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                           XXXXXXX, RGB_SPI, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI,
+          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, RGB_SPD, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD,
+                                     _______, _______, _______, _______,         _______, _______, _______, _______
       )
 };
 
@@ -187,26 +187,8 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
   }
 }
 
-extern void rgb_matrix_update_pwm_buffers(void);
-
 void keyboard_post_init_user(void) {
     // turn off liatris leds
     setPinOutput(24);
     writePinHigh(24);
-    // startup animation
-    #ifdef JARI_CUSTOM_STARTUP
-    rgb_matrix_enable_noeeprom();
-    rgb_matrix_set_color_all(0, 0, 128);
-    int leds_per_half = RGB_MATRIX_LED_COUNT / 2;
-    for (int i=0; i<leds_per_half; i++) {
-        rgb_matrix_set_color(i, 128, 0, 0);
-        rgb_matrix_set_color(i + leds_per_half, 128, 0, 0);
-        rgb_matrix_update_pwm_buffers();
-        wait_ms(50);
-        rgb_matrix_set_color(i, 0, 0, 0);
-        rgb_matrix_set_color(i + leds_per_half, 0, 0, 0);
-        rgb_matrix_update_pwm_buffers();
-    }
-    wait_ms(100);
-    #endif
 }
